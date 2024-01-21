cb8dad44ddf4ad378803a6214427a3b8bc5aa2b0
Kunal T <kunaltembhare003@gmaail.com>
2024-01-21 13:27:17 +0530

    use marshmellow to input validation

--- app.py
+++ app.py
@@ -1,26 +1,47 @@
 from flask import Flask, request, jsonify
 from marshmallow import ValidationError
 from schemas import FizzBuzzSchema
+#from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 fizz_buzz_schema = FizzBuzzSchema()
 # Store statistics
 request_stats = {}
+#app.config["JWT_SECRET_KEY"] = "kunal_learning"
+#jwt= JWTManager(app)
+
+'''@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    # Validate the username and password (this is just an example, use a secure method)
+    if username == 'your_username' and password == 'your_password':
+        # Create an access token
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Invalid credentials"}), 401'''
 
 @app.route('/fizzbuzz', methods=['GET'])
+#@jwt_required()
 def fizzbuzz():
     try:
         data = fizz_buzz_schema.load(request.args)
     except ValidationError as e:
         return jsonify({"error": "Input validation failed", "details": e.messages}), 400
 
-
     int1 = data['int1']
     int2 = data['int2']
     limit = data['limit']
     str1 = data['str1']
     str2 = data['str2']
-
+    '''data = request.get_json()
+    int1 = data.get('int1', 3)
+    int2 = data.get('int2', 5)
+    limit = data.get('limit', 100)
+    str1 = data.get('str1', 'fizz')
+    str2 = data.get('str2', 'buzz')'''
     result = [
         str1 + str2 if i % int1 == 0 and i % int2 == 0 else
         str1 if i % int1 == 0 else
@@ -36,6 +57,7 @@ def fizzbuzz():
     return jsonify(result)
 
 @app.route('/statistics', methods=['GET'])
+#@jwt_required()
 def statistics():
     if not request_stats:
         return jsonify({"message": "No statistics available"})
